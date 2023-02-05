f061cb0873019d40738271b3c5b73b1f03c72e30
ianbokser <chulygames@hotmail.com>
2023-02-05 08:03:07 -0300

    new options

--- main.py
+++ main.py
@@ -5,37 +5,74 @@ from utils import *
 import os
 from dotenv import load_dotenv
 import time
+import datetime
+import asyncio
+import random
+from discord.ext import commands 
 load_dotenv()
+
 
 class MyClient(discord.Client):
     async def on_ready(self):
         print(f'Logged on as {self.user}!')
+    
+        client = commands.Bot(command_prefix = "+", help_command=None)
+
+        @client.command()
+        async def test(ctx, role: discord.Role, user: discord.Member):
+            await user.add_roles(role)
 
     async def on_message(self, message):
         if not message.author.bot and message.content.startswith("+g"):
             command = get_command(message.content)
+            print("sexo2")
+            # crear perfil del jugador con descripcion y su imagen
+
             if command == "jugador":
                 data= parse_message(message.content)
                 create(data["name"], data["description"], data["image"])
                 await message.reply(f"se creo el jugador con el nombre {data['name']}")
                 print(readById(1))
+
+            # crear partido que se vaya a jugar, incluyendo a que hora, cantidad de jugadores, y el dia
+
             elif command == "partido":
                 datas= game(message.content)
                 create_games(datas["time"], datas["num_players"], datas["day"])
                 await message.reply(f"quienes iran al partido? \n{players_str(read())}")
-                pedo = []
-                for f in range (int(datas["num_players"])):
-                    time.sleep(5)
-                    if command == str(f):
-                        time.sleep(5)
-                        pedo.append(f)
-                    print(pedo)
                 await message.reply(f"se creo el partido el {datas['day']} a las {datas['time']}")
+
+            #  ver lista de todos los jugadores
+
             elif command == "jugadores":
                 lista = players_str(read())
                 await message.reply(lista)
-            elif command == "help":
-                await message.reply(player_id(3))
+            
+            # borrar todos los mensajes del chat
+
+            elif command == "clean":
+                pass
+
+            # colocar audio en chat de voz
+
+            elif command == "audio":
+                if message.author.voice:
+                    voice_channel = message.author.voice.channel
+                    vc = await voice_channel.connect()
+                    vc.play(discord.FFmpegPCMAudio(audios()), after=lambda e: print('done', e))
+                    vc.source = discord.PCMVolumeTransformer(vc.source)
+                    vc.source.volume = 1
+                    await asyncio.sleep(7)
+                    await vc.disconnect()
+                else:
+                    await message.channel.send("primero debes entrar a un canal de voz!")    
+
+            elif command == "ja":
+                await message.reply("+p $add-money-role @PASAJEROS  5000000")
+
+            else:
+                await message.channel.send("ese comando no existe.")
+            
 
 intents = discord.Intents.default()
 intents.message_content = True
--- utils.py
+++ utils.py
@@ -4,25 +4,14 @@ def get_command(message):
     prefix = "+g"
     if not message.startswith(prefix):
         return None
-    # Split the message into a list containing the command and its arguments
     cmd_and_args = message[len(prefix):].split()
-    # Return the command (first element in the list)
     return cmd_and_args[0] if cmd_and_args else None
     
 def parse_message(message):
-    # Split the message into a list by the '-' character
     parts = message.split('-')
-
-    # Get the name by removing the leading and trailing whitespace from the first element in the list
     name = parts[0].strip().split("+g jugador ")[1]
-
-    # Get the description by removing the leading and trailing whitespace from the second element in the list
     description = parts[1].strip()
-
-    # Get the image by removing the leading and trailing whitespace from the third element in the list
     image = parts[2].strip()
-
-    # Return the name, description, and image as an object
     return {
         'name': name,
         'description': description,
@@ -68,22 +57,56 @@ def list_players_match(id):
 
 def game(message):
     print("games")
-    # Split the message into a list by the '-' character
     parts = message.split('-')
-
-    # Get the name by removing the leading and trailing whitespace from the first element in the list
     time = parts[0].strip().split("+g partido ")[1]
-
-    # Get the description by removing the leading and trailing whitespace from the second element in the list
     num_players = parts[1].strip()
     print(num_players)
-
-    # Get the image by removing the leading and trailing whitespace from the third element in the list
     day = parts[2].strip()
-
-    # Return the name, description, and image as an object
     return {
         'time': time,
         'num_players': num_players,
         'day': day
     }
+
+def audios():
+    import random
+    audio_1 = "audios/partido/partido_1.mp3"
+    audio_2 = "audios/partido/partido_1.mp3"
+    audio_3 = "audios/partido/partido_2.mp3"
+    a = random.randint(1,3)
+    if a == 1:
+        return audio_1
+    elif a == 2:
+        return audio_2
+    else:
+        return audio_3
+
+def audio():
+    import random
+    audio_1 = "audios/random/audio_1.mp3"
+    audio_2 = "audios/random/audio_2.mp3"
+    audio_3 = "audios/random/audio_3.mp3"
+    audio_4 = "audios/random/audio_4.mp3"
+    audio_5 = "audios/random/audio_5.mp3"
+    audio_6 = "audios/random/audio_6.mp3"
+    audio_7 = "audios/random/audio_7.mp3"
+
+    a = random.randint(1,6)
+    if a == 1:
+        return audio_1
+    elif a == 2:
+        return audio_2
+    elif a == 3:
+        return audio_3
+    elif a == 4:
+        return audio_4
+    elif a == 5:
+        return audio_5
+    elif a == 6:
+        return audio_6
+    else:
+        return audio_7
+
+async def clear(ctx):
+    channel = ctx.channel
+    await channel.purge()
